4e2cef30fc5ad2c420b3078f8b0b74b1cc4be68e
Will Ernest <williamernest@google.com>
2018-09-27 16:02:42 -0700

    Fix card icon toggle

--- src/app/theme-page/theme-page.component.ts
+++ src/app/theme-page/theme-page.component.ts
@@ -17,31 +17,48 @@ import {MDCDrawer} from '@material/drawer';
 export class ThemePageComponent implements OnInit, AfterViewInit, OnDestroy {
 
   destroyableElements: Array<MDCRipple> = [];
+  elementsToInit = [
+    '.mdc-button',
+    '.mdc-fab',
+    '.mdc-icon-button',
+    '.mdc-slider',
+    '.mdc-switch',
+    '.mdc-top-app-bar',
+    '.mdc-text-field',
+    '.mdc-radio',
+    '.mdc-checkbox',
+    '.mdc-drawer',
+  ];
   constructor(private myElement: ElementRef) { }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
-    this.destroyableElements = Array.from(this.myElement.nativeElement.querySelectorAll('.mdc-button, .mdc-fab, .mdc-icon-button, .mdc-slider, .mdc-switch, .mdc-top-app-bar, .mdc-text-field, .mdc-radio, .mdc-checkbox, .mdc-drawer')).map((ele: HTMLElement) => {
-      if (ele.classList.contains('mdc-text-field')) {
-        return new MDCTextField(ele);
-      } else if (ele.classList.contains('mdc-radio')) {
-        return new MDCRadio(ele);
-      } else if (ele.classList.contains('mdc-checkbox')) {
-        return new MDCCheckbox(ele);
-      } else if (ele.classList.contains('mdc-top-app-bar')) {
-        return new MDCTopAppBar(ele);
-      } else if (ele.classList.contains('mdc-slider')) {
-        return new MDCSlider(ele);
-      } else if (ele.classList.contains('mdc-drawer')) {
-        return new MDCDrawer(ele);
-      } else if (ele.classList.contains('mdc-switch')) {
-        return new MDCSwitch(ele);
-      } else {
-        return new MDCRipple(ele);
-      }
-    });
+    this.destroyableElements = Array.from(this.myElement.nativeElement.querySelectorAll(this.elementsToInit.join((','))))
+      .map((ele: HTMLElement) => {
+        if (ele.classList.contains('mdc-text-field')) {
+          return new MDCTextField(ele);
+        } else if (ele.classList.contains('mdc-radio')) {
+          return new MDCRadio(ele);
+        } else if (ele.classList.contains('mdc-checkbox')) {
+          return new MDCCheckbox(ele);
+        } else if (ele.classList.contains('mdc-top-app-bar')) {
+          return new MDCTopAppBar(ele);
+        } else if (ele.classList.contains('mdc-slider')) {
+          return new MDCSlider(ele);
+        } else if (ele.classList.contains('mdc-drawer')) {
+          return new MDCDrawer(ele);
+        } else if (ele.classList.contains('mdc-switch')) {
+          return new MDCSwitch(ele);
+        } else if (ele.classList.contains('mdc-icon-button')) {
+          const ripple = new MDCRipple(ele);
+          ripple.unbounded = true;
+          return ripple;
+        } else {
+          return new MDCRipple(ele);
+        }
+      });
 
   }
 
